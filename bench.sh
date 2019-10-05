30c929ec42b9f16b9cf1204fa892d4e71fcad1c8
Sebastian Verschoor <sebastian@zeroknowledge.me>
2019-10-05 15:52:21 -0400

    Updated measurements

--- bench.sh
+++ bench.sh
@@ -8,11 +8,11 @@ DIR=./bench/
 
 # do fewer runs for larger numbers
 # these are somewhat arbitrary
-BITS=(8 11 18 20 9999999)
-SEEDS=(64 8 4 1)
-NUMBERS=(32 8 4 1)
+BITS=(8 12 16 18 20 9999999)
+SEEDS=(64 16 4 2 1)
+NUMBERS=(32 8 4 2 1)
 
-i=3
+i=0
 nbits=${BITS[$i]}
 nseeds=${SEEDS[$i]}
 nnumbers=${NUMBERS[$i]}
@@ -21,30 +21,32 @@ trap "exit" INT
 while true; do
     # bounds for numbers with exactly n bits
     lower=$((1 << ($nbits - 1) ))
-    upper=$((1 << $nbits))
+    upper=$(((1 << $nbits) - 1))
     if [ "$nbits" -eq "${BITS[$i]}" ]; then
         nseeds=${SEEDS[$i]}
         nnumbers=${NUMBERS[$i]}
         i=$(($i + 1))
     fi
-    echo $nbits, $nseeds, $nnumbers: $lower -- $upper
+    echo "n: $nbits, count: $nnumbers, seeds: $nseeds, range: [$lower, $upper]"
 
     # generate $nnumbers numbers with n bits
     trap "exit" INT
     for N in $(shuf -i $lower-$upper -n $nnumbers); do
         # generate the Circuit-SAT instance
         $GENERATOR $N >"${DIR}${N}.dimacs"
-        echo $N generated
+        printf "${N}"
         mkdir "${DIR}sol_${N}"
 
         # solve it $nseeds times
         trap "exit" INT
         for seed in $(seq 1 $nseeds); do
+            printf "."
             start=$(date +%s.%N)
             $SATSOLVER -rnd-init -rnd-seed=$seed <"${DIR}${N}.dimacs" >"${DIR}sol_${N}/${seed}.dimacs"
             end=$(date +%s.%N)
             echo $N $seed $(echo $end - $start | bc) >> timing.txt
         done
+        printf "\n"
     done
     nbits=$(($nbits+1))
 done
